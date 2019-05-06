3bca7035641fd42b09fa5488abfe0691775137d3
Kashif Javed <kash.animations@gmail.com>
2019-05-06 13:22:18 +0000

    Add requirements.txt and connect to database in app.py

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
 import os
 from flask import Flask
+import pymysql
+
+# Get the MySQL username from C9
+username = os.getenv('C9_USER')
+
+# Connect to the Recipes database
+connection = pymysql.connect(host='localhost',
+                            user=username,
+                            password='',
+                            db='Recipes')
+                            
