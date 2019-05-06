a744267c1fb854256ba27ecead05ce6cb381ce05
Kashif Javed <kash.animations@gmail.com>
2019-05-06 13:27:55 +0000

    Add Procfile, include more features from Flask

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
-from flask import Flask
+from flask import Flask, redirect, request, render_template, url_for
 import pymysql
+
+app = Flask(__name__)
 
 # Get the MySQL username from C9
 username = os.getenv('C9_USER')
@@ -11,3 +13,10 @@ connection = pymysql.connect(host='localhost',
                             password='',
                             db='Recipes')
                             
+
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+        port=int(os.environ.get('PORT')),
+        debug=True)
