feead8630c90a905e4e23f2e9f9021588e72eca7
Kashif Javed <kash.animations@gmail.com>
2019-05-07 18:08:22 +0000

    Add functionality to log in with username if found on the datatbase. Add blank style.css to prevent console warnings

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
-from flask import Flask, redirect, request, render_template, url_for
+from flask import Flask, redirect, request, render_template, url_for, flash
 import pymysql
 
 app = Flask(__name__)
+app.secret_key = 'secret_key'
 
 # Get the MySQL username from C9
 username = os.getenv('C9_USER')
@@ -43,8 +44,40 @@ def insert_user():
     
     # Go back to the login page
     return redirect(url_for('login'))
-        
+
+
+@app.route('/get_user', methods=['POST'])
+def get_user():
     
+    # Store username entered by the user in a variable
+    form_username = request.form.get('username')
+    
+    with connection.cursor() as cursor:
+            
+        cursor.execute('SELECT * FROM User WHERE Username = %s;', form_username.lower())
+        result = cursor.fetchone()
+        
+        if result != None:
+            # If it does, redirect to the user page
+            return redirect(url_for('user_page', username=form_username))
+                
+        else:
+            # If not, display Flash message and return to the login page
+            flash('Sorry, this is not a valid username')
+            return redirect(url_for('login'))
+
+@app.route('/user_page/<username>')
+def user_page(username):
+    
+    with connection.cursor() as cursor:
+        
+        # Search for the full user data to pass to 'user'html'
+        cursor.execute('SELECT * FROM User WHERE Username = %s;', username.lower())
+        result = cursor.fetchone()
+        
+    return render_template('user.html', user_data=result)
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
